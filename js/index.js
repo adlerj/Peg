de1a41b07ea542d135bcfac12386fec4223f1d32
adlerj <jultimate123@gmail.com>
2014-01-13 16:59:32 -0500

    style changes

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $('document').ready(function() {
-        var options = { videoId: 'GB4iPT_BaBY', start: 35 };
+        var options = { videoId: 'qREKP9oijWI', start: 20 };
         $('#wrapper').tubular(options);
         // f-UGhWj1xww cool sepia hd
         // 49SKbS7Xwf4 beautiful barn sepia
